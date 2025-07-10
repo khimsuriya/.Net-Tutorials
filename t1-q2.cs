c23972248a9bc9276f4e4a6f0bc32e7fc25ed974
Kinjal Khimsuriya <kkhimsuriya805@rku.ac.in>
2025-07-10 15:18:52 +0530

    q2

---
+++ t1-q2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    internal class t1_q2
+    {
+        static void Main(String[] arg)
+        {
+            Console.WriteLine("Name: KHIMSURIYA KINJAL");
+            Console.WriteLine("DOB: 16/09/2006");
+            Console.WriteLine("Address: Thorala");
+            Console.WriteLine("City: Rajkot");
+            Console.WriteLine("Pincode: 360 003");
+            Console.WriteLine("state: Gujrat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: kkhimsuriya805@rku.ac.in");
+        }
+
+    }
+}
