1a2d4a5a0ed9f1a2ad6960f225ae9429bf526074
Kinjal Khimsuriya <kkhimsuriya805@rku.ac.in>
2025-07-10 15:16:12 +0530

    Add project files.

---
+++ t1-q1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    internal class t1_q1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("24SOECE13032 -  KHIMSURIYA KINJAL");
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
